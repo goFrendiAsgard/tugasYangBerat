c0bed5c5a191c9d43b8061d8288535e04d674659
goFrendiAsgard <gofrendiasgard@gmail.com>
2018-09-27 17:17:27 +0700

    init

---
+++ TugasYangBerat.java
@@ -0,0 +1,42 @@
+class Orang {
+    public String name;
+    public Orang next;
+}
+public class TugasYangBerat {
+    static Orang terdepan;
+    public static void main(String[] args) {
+        terdepan = new Orang();
+        terdepan.name = "Bima";
+        terdepan.next = new Orang();
+        terdepan.next.name = "Bimo";
+        removeFirst();
+        addFirst("Ana");
+        removeLast();
+        addLast("Chika");
+        show(); // Ana Chika
+    }
+
+    public static void show() {
+        Orang orang = terdepan;
+        while(orang != null) {
+            System.out.print(orang.name + " ");
+            orang = orang.next;
+        }
+        System.out.println();
+    }
+
+    public static void addFirst(String nama) {
+        Orang orang = new Orang();
+        orang.name = nama;
+        orang.next = terdepan;
+        terdepan = orang;
+    }
+
+    public static void removeFirst() {
+        terdepan = terdepan.next;
+    }
+
+    public static void addLast(String nama) {}
+    public static void removeLast() {}
+
+}
